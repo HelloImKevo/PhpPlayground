817c6b9a8e8f26dc2497fdbfadaef26ede981496
Kevin Schanz <kschanz@imobile3.com>
2020-05-01 18:40:11 -0400

    PluralSight: Fun with strings (yey)

--- src/BasicExamples.php
+++ src/BasicExamples.php
@@ -88,3 +88,84 @@ echo "\n";
 $variableFunctionName = "getAuthor";
 // Invoke the variable function.
 echo $variableFunctionName();
+
+$currency = 'penny';
+// Because of single quotes, this will print: 
+// A $currency saved is a $currency earned.
+$sampleString = 'A $currency saved is a $currency earned.';
+echo $sampleString;
+
+function funWithStrings() {
+    $var = 2;
+    echo "\n";
+    echo "{$var}nd place" . "\n";
+    echo $var . "nd place" . "\n";
+    echo "St. Patrick's Day" . "\n";
+}
+
+funWithStrings();
+
+// "Here Document" example - End of Text
+echo <<<EOT
+    Be not afraid of greatness;
+    some are born great,
+    some achieve greatness,
+    and others have greatness thrust upon them.
+
+                    - William Shakespeare
+EOT;
+
+print("\nThis above all: to thine own self be true\n");
+echo "Et", " tu", ", ", "Brute", "!", "\n";
+
+$quote = strtolower("To be or not to be, that is the Question.");
+echo $quote . "\n";
+
+// String concat example from: 
+// https://electrictoolbox.com/php-echo-commas-vs-concatenation/
+$string1 = str_repeat('x', 100);
+$string2 = str_repeat('y', 100);
+$string3 = str_repeat('z', 100);
+
+$mark = microtime(true);
+
+for ($i = 0; $i < 2000; $i++) {
+    // Turn on output buffering
+    ob_start();
+    for ($j = 0; $j < 2000; $j++) {
+        print($string1 . $string2 . $string3);
+    }
+    // Get current buffer contents and delete current output buffer
+    ob_get_clean();
+}
+
+// This demo shows that echo with comma separated args is slightly faster
+// (but the real world difference in performance is infintisimal).
+echo microtime(true) - $mark;
+
+$quote2 = "Courage is resistance to fear, mastery of fear, "
+        . "not absence of fear.";
+
+echo "\n'fear' string pos: ", strpos($quote2, "fear", 26);
+echo "\n'm' string pos: ", strpos($quote2, "m");
+echo "\n'z' string pos: ", strpos($quote2, "z");
+
+$quote3 = "To be or not to be? That is a question.";
+
+// Pass by reference example for 'count' argument.
+// Method Signature:
+// function str_replace($search, $replace, $subject, int &$count = NULL) {}
+$replaced = str_replace("be", "know", $quote3, $count);
+echo "\nReplaced string: ", $replaced;
+echo "\nHow many instances replaced: ", $count;
+
+$quote4 = "Only those who will risk going too far can possibly find out "
+        . "how far one can go.";
+echo "\nLength: ", strlen($quote4);
+echo "\nSubstring: ", substr($quote4, 3, 5);
+echo "\nSubstring with negative index: ", substr($quote4, -3);
+
+$quote5 = "It is a melancholoy truth that even great men have their poor "
+        . "relations.";
+$varArray = str_split($quote5, 8);
+print_r($varArray);
